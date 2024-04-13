5ddd26dd6c6d447b42786a564dffd8a1545b9e2b
Nicholas Robinson <kylenr23@gmail.com>
2024-04-13 15:31:33 +0100

    ADD Storage Object and Ingredient Randomisation

--- scenes/items/ingredient.gd
+++ scenes/items/ingredient.gd
@@ -12,13 +12,18 @@ enum IngredientTypes {
 }
 @export var type: IngredientTypes
 @export var potency: int = -1
+@export var randomise_type: bool = false
 
 @export_category("Components")
 @export var sprite: AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if randomise_type:
+		randomise()
 	sprite.frame = type
 	if potency == -1:
 		potency = randi_range(10,20)
 
+func randomise():
+	type = IngredientTypes.values()[randi_range(0,IngredientTypes.size()-1)]
