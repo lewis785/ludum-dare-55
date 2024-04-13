e0fadeda94b8ef69d924c8a7ea2d2314f1756643
Jack Foster <fosterjack794@gmail.com>
2024-04-13 15:05:55 +0100

    ADD torches with life animations adn tidy up code

--- scenes/levels/sliding_window.gd
+++ scenes/levels/sliding_window.gd
@@ -1,34 +1,26 @@
 extends Node2D
 
 @onready var camera = $Camera2D
+@onready var torch_animation = $TorchAnimation
 
-
-@export var roomNumber: int
 @export var cameraSpeed: int
-var roomScene = load("res://scenes/levels/room.tscn")
+@export var lives: int
 
+var roomScene = load("res://scenes/levels/room.tscn")
+var torchScene = load("res://scenes/props/torch.tscn")
 
 var target = 0
 var target_coordinates = Vector2(400,225)	#Initialise the variable so it doesnt complain
 var camera_coordinates = Vector2(400,225)
-var rooms: Array[PackedScene] = []
+#: Array[PackedScene] = []
 
 #Camera set to 1.44
 func _ready():
-	print("creating room ")
-	for i in range(roomNumber):
-		#print(i)
-		var instance = roomScene.instantiate()
-		instance.position = Vector2((800*i), 0)
-		rooms.push_front(instance)
-		add_child(instance)
+	_createNextRoom(0)
 		
-func _input(event):
-	print(event)
+func _input(_event):
 	if(Input.is_key_pressed(KEY_ENTER)):	#UPDATE WHEN BATTLE ENDS
-		if(target<=roomNumber-2):
-			#print(target)
-			#print(roomNumber)
+			_createNextRoom(target+1)
 			target+=1
 			target_coordinates = Vector2(400+(800*target), 225)
 
@@ -37,3 +29,29 @@ func _process(delta):
 	camera_coordinates = camera.get_position()
 	camera.position = camera_coordinates.lerp(target_coordinates, cameraSpeed*delta)
 	camera.set_position(camera.position)
+
+func _createNextRoom(nextRoom):
+		var roomInstance = roomScene.instantiate()
+		roomInstance.position = Vector2((800*nextRoom), 0)
+		add_child(roomInstance)
+		
+		for x in range(3):
+			#print("creating torch")
+			var torchInstance: Node2D = torchScene.instantiate()
+			add_child(torchInstance)
+			
+			if(x == 0):
+				torchInstance.position = Vector2(110+(800*nextRoom), 175)
+				torchInstance.set_rotation_degrees(-15)
+				if(lives < 2):
+					torchInstance.find_child("TorchAnimation").animation = "notLitFam"
+			if(x == 1):
+				torchInstance.position = Vector2(400+(800*nextRoom), 100)
+				if(lives < 1):
+					torchInstance.find_child("TorchAnimation").animation = "notLitFam"
+			if(x == 2):
+				if(lives < 3):
+					torchInstance.find_child("TorchAnimation").animation = "notLitFam"
+				torchInstance.position = Vector2(675+(800*nextRoom), 175)
+				torchInstance.set_rotation_degrees(15)
+	
