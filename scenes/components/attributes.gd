84a57f8907cc063524195abe1efb020dca5489f5
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-13 11:09:21 +0100

    ADD attributes class name

--- scenes/components/attributes.gd
+++ scenes/components/attributes.gd
@@ -1,16 +1,13 @@
 extends Node
 
+class_name Attributes
+
 @export_group("Attributes")
 @export_range(0, 100) var defence = 0:
 	set(value):
 		defence = clamp(value, 0, 100)
 	get:
 		defence
-@export_range(0, 100) var vitality = 0:
-	set(value):
-		vitality = clamp(value, 0, 100)
-	get:
-		vitality
 @export_range(0, 100) var luck = 0:
 	set(value):
 		luck = clamp(value, 0, 100)
@@ -26,6 +23,11 @@ extends Node
 		strength = clamp(value, 0, 100)
 	get:
 		strength
+@export_range(0, 100) var vitality = 0:
+	set(value):
+		vitality = clamp(value, 0, 100)
+	get:
+		vitality
 		
 @export_group("Resistances")
 @export_range(-100, 100) var slashing = 0:
