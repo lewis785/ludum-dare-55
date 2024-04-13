fcbe97c7d68786ae55e49ee96c594c4467ac4ba9
Nicholas Robinson <kylenr23@gmail.com>
2024-04-13 17:08:58 +0100

    ADD Ingredient removal post summon

--- scenes/items/summoning_circle.gd
+++ scenes/items/summoning_circle.gd
@@ -1,17 +1,23 @@
 extends Area2D
 
-@export var ingredients = []
 @export var output_stats: AttributesComponent
+@export var ingredients_cap : int = 5
+
+@export var creature_summon : Character
+
+var ingredients = []
 
 # Called when the node enters thegit c scene tree for the first time.
 func _ready():
-	ingredients.clear()    
+	ingredients.clear()
+
+func clear_stats():
 	output_stats.defence = 0
 	output_stats.luck = 0
 	output_stats.speed = 0
 	output_stats.strength = 0
 	output_stats.vitality = 0
-	
+
 func add_ingredient(ingr):
 	ingredients.append(ingr)
 	
@@ -19,6 +25,8 @@ func remove_ingredient(ingr):
 	ingredients.erase(ingr)
 
 func combine_ingredients():
+	if ingredients.size() > ingredients_cap:
+		return
 	var stats = [0,0,0,0,0]
 	
 	for ingr : Ingredient in ingredients:
@@ -28,7 +36,15 @@ func combine_ingredients():
 	output_stats.luck = stats[1]
 	output_stats.speed = stats[2] 
 	output_stats.vitality = stats[4]
-
+	
+	# Summon creature with new stats
+	creature_summon.visible = false
+	creature_summon.position = position
+	creature_summon.attributes_component = output_stats
+	creature_summon.visible = true
+	
+	for ingr : Ingredient in ingredients:
+		ingr.queue_free()
 
 func _on_area_entered(area):
 	if area.is_in_group("Ingredient"):   
