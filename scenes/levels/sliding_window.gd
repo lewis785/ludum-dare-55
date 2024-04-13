ed93ba44f3c0ca8dd9a8a30567a73d2539065809
Jack Foster <fosterjack794@gmail.com>
2024-04-13 11:57:29 +0100

    EDIT room scene - update to auto generate and move camera between.

--- scenes/levels/sliding_window.gd
+++ scenes/levels/sliding_window.gd
@@ -1,45 +1,39 @@
 extends Node2D
 
 @onready var camera = $Camera2D
-@onready var snap_point_0 = $CameraSnaps/SnapPoint0
-@onready var snap_point_1 = $CameraSnaps/SnapPoint1
-@onready var snap_point_2 = $CameraSnaps/SnapPoint2
-@onready var snap_point_3 = $CameraSnaps/SnapPoint3
-@onready var snap_point_4 = $CameraSnaps/SnapPoint4
-@onready var snap_point_5 = $CameraSnaps/SnapPoint5
-@onready var snap_point_6 = $CameraSnaps/SnapPoint6
 
-const ROOM0 = 0
-const ROOM1 = 1
-const ROOM2 = 2
-const ROOM3 = 3
-const ROOM4 = 4
-const ROOM5 = 5
-const ROOM6 = 6
+
+@export var roomNumber: int
+@export var cameraSpeed: int
+var roomScene = load("res://scenes/levels/room.tscn")
+
 
 var target = 0
-var target_coordinates = Vector2.ZERO	#Initialise the variable so it doesnt complain
+var target_coordinates = Vector2(400,225)	#Initialise the variable so it doesnt complain
+var camera_coordinates = Vector2(400,225)
+var rooms: Array[PackedScene] = []
 
+#Camera set to 1.44
+func _ready():
+	print("creating room ")
+	for i in range(roomNumber):
+		print(i)
+		var instance = roomScene.instantiate()
+		instance.position = Vector2((800*i), 0)
+		rooms.push_front(instance)
+		add_child(instance)
+		
 func _input(event):
 	print(event)
-	if(Input.is_key_pressed(KEY_ENTER)):
-		target+=1
-		
-		match target:
-			ROOM0:
-				target_coordinates = snap_point_0.get_position()
-			ROOM1:
-				target_coordinates = snap_point_1.get_position()
-			ROOM2:
-				target_coordinates = snap_point_2.get_position()
-			ROOM3:
-				target_coordinates = snap_point_3.get_position()
-			ROOM4:
-				target_coordinates = snap_point_4.get_position()
-			ROOM5:
-				target_coordinates = snap_point_5.get_position()
-			ROOM6:
-				target_coordinates = snap_point_6.get_position()
-		print(target_coordinates)
-	
-		camera.set_position(target_coordinates)
+	if(Input.is_key_pressed(KEY_ENTER)):	#UPDATE WHEN BATTLE ENDS
+		if(target<=roomNumber-2):
+			print(target)
+			print(roomNumber)
+			target+=1
+			target_coordinates = Vector2(400+(800*target), 225)
+
+
+func _process(delta):
+	camera_coordinates = camera.get_position()
+	camera.position = camera_coordinates.lerp(target_coordinates, cameraSpeed*delta)
+	camera.set_position(camera.position)
