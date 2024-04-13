9be25ae58443dbfda5942b230fa266a5c4a24e7d
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-13 17:12:10 +0100

    ADD character attack component

---
+++ scenes/components/character/attack_component.gd
@@ -0,0 +1,34 @@
+extends Node
+
+class_name AttackComponent
+
+@export var attributes_component: AttributesComponent
+@export var base_attack_rate = 60
+var attack_rate: float
+
+func _ready():
+	attack_rate = ((100 - attributes_component.speed) / 100.0) * base_attack_rate
+	print("Attack Rate: " + str(attack_rate))
+
+func attack() -> Attack:
+	var attack = Attack.new()
+	attack.damage = _damage_amount()
+	attack.type = AttributesComponent.DamageTypes.SLASHING
+	
+	return attack
+
+func _damage_amount():
+	var strength = attributes_component.strength
+	
+	if strength >=  90:
+		return randi() % 10 + 50
+	if strength >=  70:
+		return randi() % 10 + 40
+	if strength >=  50:
+		return randi() % 10 + 30
+	if strength >=  30:
+		return randi() % 10 + 20
+	if strength >=  10:
+		return randi() % 10 + 10
+		
+	return randi() % 10
