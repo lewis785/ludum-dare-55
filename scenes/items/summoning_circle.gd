366160b7914569150dcb736f7c83c22c745c0aee
Nicholas Robinson <kylenr23@gmail.com>
2024-04-13 13:32:20 +0100

    FIX Draggable Component name format

--- scenes/items/summoning_circle.gd
+++ scenes/items/summoning_circle.gd
@@ -6,7 +6,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ingredients = []
+	ingredients.clear()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
