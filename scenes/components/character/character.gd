37bb08f35ca250c7b27d477c356f992b797d2042
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-13 20:58:42 +0100

    ADD attack component to character scene

--- scenes/components/character/character.gd
+++ scenes/components/character/character.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 class_name Character
 
-@export var cb: CharacterBody2D
+@export var character_body: CharacterBody2D
 
+@onready var attack_component: AttackComponent = $AttackComponent
 @onready var attributes_component : AttributesComponent = $AttributesComponent
 @onready var health_component : HealthComponent = $HealthComponent
-
