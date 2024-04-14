29dca80350a24a274e3328dc4f3dcc469cd33700
Nicholas Robinson <kylenr23@gmail.com>
2024-04-14 18:51:19 +0100

    ADD Raise/Lower Book

--- scenes/ui/summon_book.gd
+++ scenes/ui/summon_book.gd
@@ -1,18 +1,47 @@
 extends Node2D
 
 @export var summoning_circle : SummoningCircle
+@export var arrow_sprite : Sprite2D
 @onready var base_x : float = summoning_circle.position.x
 @export var base_y : float = 160
-@export var speed : int = 3
-@export var height : int = 20
+@export var speed : float = 1.0
+@export var height : int = 250
 
-var tween_y : Tween
+var tween : Tween
+var book_raised : bool = false
 
 func _ready():
-	pass
+	book_raised = true
+	
+func raise_book():
+	tween = self.create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "global_position:y", -height, speed).as_relative().set_ease(Tween.EASE_IN_OUT)
+	var arrow_tween = arrow_sprite.create_tween().set_trans(Tween.TRANS_SINE)
+	arrow_tween.tween_property(arrow_sprite, "rotation_degrees", 180, speed).as_relative().set_ease(Tween.EASE_IN_OUT)
+	book_raised = true
 
+func lower_book():
+	tween = self.create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "global_position:y", height, speed).as_relative().set_ease(Tween.EASE_OUT)
+	var arrow_tween = arrow_sprite.create_tween().set_trans(Tween.TRANS_SINE)
+	arrow_tween.tween_property(arrow_sprite, "rotation_degrees", 180, speed).as_relative().set_ease(Tween.EASE_IN_OUT)
+	book_raised = false
+
+func hide_book():
+	tween = self.create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "global_position:y", 1000, speed).as_relative().set_ease(Tween.EASE_IN_OUT)
+	book_raised = false
+
+func toggle_position():
+	if book_raised:
+		lower_book()
+	else:
+		raise_book()
+	
 func _on_button_pressed():
 	if summoning_circle.is_summonable():
-		tween_y = self.create_tween().set_trans(Tween.TRANS_SINE)
-		tween_y.tween_property(self, "global_position:y", 1000, speed).as_relative().set_ease(Tween.EASE_IN)
-		
+		hide_book()
+
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		toggle_position()
