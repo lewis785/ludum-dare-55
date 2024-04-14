8a4adf802372806a10b05a21c20da20cc5b419bc
Nicholas Robinson <kylenr23@gmail.com>
2024-04-14 11:08:21 +0100

    FIX Added Continuous Room Generation

--- scenes/levels/room.gd
+++ scenes/levels/room.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
-
+class_name Room
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +10,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _setTorchesBrightness(brightness : int, amount : int = 0):
+		if brightness == 0:
+			for i in range(3):
+				var torch = find_child("Torch" + str(i))
+				torch.find_child("TorchAnimation").animation = "notLitFam"
+				torch.find_child("PointLight2D").energy = 0.0
+				torch.find_child("Extinguish").play()
+		else:
+			var torch
+			# Torches are called in odd order due to room order
+			if(amount >= 2):
+				torch = find_child("Torch0")
+				torch.find_child("TorchAnimation").animation = "litfam"
+				torch.find_child("PointLight2D").energy = brightness
+			if(amount >= 1):
+				torch = find_child("Torch1")
+				torch.find_child("TorchAnimation").animation = "litfam"
+				torch.find_child("PointLight2D").energy = brightness
+			if(amount >= 3):
+				torch = find_child("Torch2")
+				torch.find_child("TorchAnimation").animation = "litfam"
+				torch.find_child("PointLight2D").energy = brightness
--- scenes/levels/sliding_window.gd
+++ scenes/levels/sliding_window.gd
@@ -7,8 +7,10 @@ extends Node2D
 
 var roomScene = preload("res://scenes/levels/room.tscn")
 
-var roomInstance1 = roomScene.instantiate()
-var roomInstance2 = roomScene.instantiate()
+#var roomInstance1 = roomScene.instantiate()
+#var roomInstance2 = roomScene.instantiate()
+
+var room_instance : Room = roomScene.instantiate()
 
 var target_coordinates = Vector2(400,225)	#Initialise the variable so it doesnt complain
 var camera_coordinates = Vector2(400,225)
@@ -21,86 +23,88 @@ var hissSoundPlayed = 1
 var walkSoundPlayed = 1
 var enableWizard = 0
 var flag = 1;
+
 #Camera set to 1.44
 func _ready():
-	_createNextRoom(0)
+	_createNextRoom()
 	
 		
 func _input(_event):
 	if(Input.is_key_pressed(KEY_ENTER)):	#UPDATE WHEN BATTLE ENDSS
-		roomInstance1.find_child("AudioStreamPlayer2D").stop()	#Make sure to call this when battle starts!!!
-		target+=1
+		room_instance.find_child("AudioStreamPlayer2D").stop()	#Make sure to call this when battle starts!!!
+		_createNextRoom()
+		#target+=1
 
 func _process(delta):
 	camera_coordinates = camera.get_position()
 	camera.position = camera_coordinates.move_toward(target_coordinates, cameraSpeed*delta)
 	camera.set_position(camera.position)
-	_createNextRoom(target)
-	if(enableWizard == 1):
-		roomInstance1.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress+= wizardSpeed*delta
-	if(enableWizard == 2):
-		roomInstance2.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress+= wizardSpeed*delta
+	#_createNextRoom()
+	#if(enableWizard == 1):
+		#roomInstance1.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress+= wizardSpeed*delta
+	#if(enableWizard == 2):
+		#roomInstance2.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress+= wizardSpeed*delta
 		
-func _createNextRoom(nextRoom):
-	var roomInstance = roomInstance1 if nextRoom % 2 == 0 else roomInstance2
-	var notRoomInstance = roomInstance2 if nextRoom % 2 == 0 else roomInstance1
-	if (nextRoom % 2 == 0):
-		flag = 2
-	else:
-		flag = 1
-		
-	if(nextRoom > roomsMade):
-		roomInstance.position = Vector2((800*nextRoom), 0)
-		add_child(roomInstance)
-		roomsMade += 1
-		igniteSoundPlayed = 0
-		if(tempPleaseLewisDontHateMe > 0):
-			hissSoundPlayed = 0
-			walkSoundPlayed = 0
-		
-	if(nextRoom == roomsMade):
-		if(hissSoundPlayed == 0):
-			roomInstance.find_child("Torch0").find_child("Extinguish").play()
-			hissSoundPlayed = 1
-			_setTorchesBrightness(notRoomInstance, 0.0)
-		if(walkSoundPlayed == 0):
-			roomInstance.find_child("Torch0").find_child("Walking").play()
-			walkSoundPlayed = 1
-			
-			enableWizard = flag
-			notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").find_child("PointLight2D").set_enabled(true)
-			notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").visible = true
-		
-		target_coordinates = Vector2(400+(800*nextRoom), 225)
-		if(target_coordinates == camera_coordinates):
-			tempPleaseLewisDontHateMe+=1
-			if((tempPleaseLewisDontHateMe > 2) and (target_coordinates == camera_coordinates) and igniteSoundPlayed == 0):
-				enableWizard = 0
-				notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").find_child("PointLight2D").set_enabled(false)
-				notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").visible = false
-				roomInstance1.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress = 0
-				roomInstance2.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress = 0
-				roomInstance.find_child("Torch0").find_child("Ignite").play()	
-				_setTorchesBrightness(roomInstance, 10.0)
-				tempPleaseLewisDontHateMe = 0
-				roomInstance1.find_child("AudioStreamPlayer2D").play()
-				igniteSoundPlayed = 1
+func _createNextRoom():
+	var old_room : Room = room_instance
+	room_instance = roomScene.instantiate()
+	#room_instance.position = Vector2((800*target), 0)
+	room_instance.position = Vector2(old_room.position.x + 800, 0)
+	add_child(room_instance)
 	
-func _setTorchesBrightness(instance: Node, brightness):
-		if(brightness == 0.0):
-			instance.find_child("Torch0").find_child("TorchAnimation").animation = "notLitFam"
-			instance.find_child("Torch0").find_child("PointLight2D").energy = 0.0
-			instance.find_child("Torch1").find_child("TorchAnimation").animation = "notLitFam"
-			instance.find_child("Torch1").find_child("PointLight2D").energy = 0.0
-			instance.find_child("Torch2").find_child("TorchAnimation").animation = "notLitFam"
-			instance.find_child("Torch2").find_child("PointLight2D").energy = 0.0
-		else:
-			if(lives >= 2):
-				instance.find_child("Torch0").find_child("TorchAnimation").animation = "litfam"
-				instance.find_child("Torch0").find_child("PointLight2D").energy = brightness
-			if(lives >= 1):
-				instance.find_child("Torch1").find_child("TorchAnimation").animation = "litfam"
-				instance.find_child("Torch1").find_child("PointLight2D").energy = brightness
-			if(lives >= 3):
-				instance.find_child("Torch2").find_child("TorchAnimation").animation = "litfam"
-				instance.find_child("Torch2").find_child("PointLight2D").energy = brightness
+	old_room._setTorchesBrightness(0)
+	
+	target_coordinates = Vector2(400+room_instance.position.x, 225)
+	
+	room_instance._setTorchesBrightness(10,3)
+	
+	old_room.queue_free()
+	
+	
+	
+	
+	
+	#var roomInstance = roomInstance1 if nextRoom % 2 == 0 else roomInstance2
+	#var notRoomInstance = roomInstance2 if nextRoom % 2 == 0 else roomInstance1
+	#if (nextRoom % 2 == 0):
+		#flag = 2
+	#else:
+		#flag = 1
+		#
+	#if(nextRoom > roomsMade):
+		#roomInstance.position = Vector2((800*nextRoom), 0)
+		#add_child(roomInstance)
+		#roomsMade += 1
+		#igniteSoundPlayed = 0
+		#if(tempPleaseLewisDontHateMe > 0):
+			#hissSoundPlayed = 0
+			#walkSoundPlayed = 0
+		#
+	#if(nextRoom == roomsMade):
+		#if(hissSoundPlayed == 0):
+			#roomInstance.find_child("Torch0").find_child("Extinguish").play()
+			#hissSoundPlayed = 1
+			##_setTorchesBrightness(notRoomInstance, 0.0)
+		#if(walkSoundPlayed == 0):
+			#roomInstance.find_child("Torch0").find_child("Walking").play()
+			#walkSoundPlayed = 1
+			#
+			#enableWizard = flag
+			#notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").find_child("PointLight2D").set_enabled(true)
+			#notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").visible = true
+		#
+		#target_coordinates = Vector2(400+(800*nextRoom), 225)
+		#if(target_coordinates == camera_coordinates):
+			#tempPleaseLewisDontHateMe+=1
+			#if((tempPleaseLewisDontHateMe > 2) and (target_coordinates == camera_coordinates) and igniteSoundPlayed == 0):
+				#enableWizard = 0
+				#notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").find_child("PointLight2D").set_enabled(false)
+				#notRoomInstance.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").find_child("Wizard").visible = false
+				#roomInstance1.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress = 0
+				#roomInstance2.find_child("Node2D").find_child("Path2D").find_child("PathFollow2D").progress = 0
+				#roomInstance.find_child("Torch0").find_child("Ignite").play()	
+				##_setTorchesBrightness(roomInstance, 10.0)
+				#tempPleaseLewisDontHateMe = 0
+				#roomInstance1.find_child("AudioStreamPlayer2D").play()
+				#igniteSoundPlayed = 1
+
