3bcf3640b4c7dfdf984110595cf9302394021f29
Jack Foster <fosterjack794@gmail.com>
2024-04-14 14:04:45 +0100

    FIX animation adn added shafts

---
+++ scenes/props/Sigil.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+@export var brightness: float
+@export var pulseDelay: int
+@export var statNumber: int
+@export var statValue: int
+@export var midThreshold: int
+@export var highThreshold: int
+
+var counter = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (counter % pulseDelay == 0):
+		print("pulse")
+	counter+=1
