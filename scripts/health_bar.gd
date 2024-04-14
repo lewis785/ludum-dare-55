e6265ad788a7e24353940721efdd0750c14693f2
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-14 23:04:14 +0100

    FIX health bar now showing correct health

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,12 +1,10 @@
 extends ProgressBar
 
 @export var health_component: HealthComponent
-@onready var animated_sprite_2d = $"../AnimatedSprite2D"
 
 func _ready():
-	value = health_component.max_health
 	max_value = health_component.max_health
-
+	value = max_value
 
 func _on_health_component_damaged(damage, crit):
 	value -= damage
