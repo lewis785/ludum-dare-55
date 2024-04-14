00b36cd2195f9e44394d93baf39ac2bc8ef15166
Jack Foster <fosterjack794@gmail.com>
2024-04-14 19:33:32 +0100

    ADD wizard thumb

--- wizard_arm.gd
+++ wizard_arm.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 const SPEED = 200 # Set a higher speed to follow the mouse faster
 @onready var wizard_arm = $"."
+@onready var arm_visual = $ArmVisual
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,9 @@ func _process(delta):
 	
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
+	
+func _input(_event):
+	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		arm_visual.animation = "Thumb"
+	if(not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		arm_visual.animation = "default"
