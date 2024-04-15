5fbca0e0e2ca5a590098e3af08cb5e54a2f085c3
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-15 20:22:59 +0100

    ADD how to play page

--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var cinematic = $TextureRect/Cinematic
 @onready var start_button = $TextureRect/StartButton
 @onready var credits_button = $TextureRect/CreditsButton
+@onready var instruction_page = $InstructionPage
 @onready var quit_button = $TextureRect/QuitButton
 @onready var how_to_button = $TextureRect/HowToButton
 @onready var credits_page = $CreditsPage
@@ -29,11 +30,14 @@ func _on_start_button_pressed():
 
 func _on_main_menu_button_pressed():
 	credits_page.visible = false
+	instruction_page.visible = false
 
 
 func _on_credits_button_pressed():
 	credits_page.visible = true
 
+func _on_how_to_button_pressed():
+	instruction_page.visible = true
 
 func _on_quit_button_pressed():
 	get_tree().quit()
