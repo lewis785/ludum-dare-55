35b80ac62bcf7afa439762868608e63f4c92f08d
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-04-15 01:29:05 +0100

    ADD character attack animation

--- scenes/components/character/attack_component.gd
+++ scenes/components/character/attack_component.gd
@@ -4,11 +4,14 @@ class_name AttackComponent
 
 @export var attributes_component: AttributesComponent
 @export var base_attack_rate = 60
+@export var sprit: AnimatedSprite2D
+
+enum AttackDirection { LEFT, RIGHT}
+@export var attack_direction: AttackDirection = AttackDirection.RIGHT
 var attack_rate: float
 
 func _ready():
 	attack_rate = ((100 - attributes_component.speed) / 100.0) * base_attack_rate
-	print("Attack Rate: " + str(attack_rate))
 
 func attack() -> Attack:
 	var attack = Attack.new()
@@ -16,7 +19,15 @@ func attack() -> Attack:
 	attack.type = AttributesComponent.DamageTypes.SLASHING
 	attack.crit = randi() % 100 < attributes_component.luck
 	
+	_attack_movement()
 	return attack
+
+func _attack_movement():
+	var attack_vector = Vector2(20, 0) if attack_direction == AttackDirection.RIGHT else Vector2(-20, 0)
+	var tween = create_tween()
+	
+	tween.tween_property(sprit, "position", attack_vector, 0.05)
+	tween.tween_property(sprit, "position", Vector2(0, 0), 0.05)
 
 func _damage_amount():
 	var strength = attributes_component.strength
