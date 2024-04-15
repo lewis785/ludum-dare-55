59582be74badb8763a2761d4ad3bb5e3ae7b6e4d
Nicholas Robinson <kylenr23@gmail.com>
2024-04-15 01:34:13 +0100

    ADD Wizard book hand

--- scenes/levels/HandManager.gd
+++ scenes/levels/HandManager.gd
@@ -1,34 +1,30 @@
 extends Node2D
 
 @onready var wizard_arm = $WizardArm
-@onready var book_wizard_arm = $BookWizardArm
+#@onready var book_wizard_arm = $BookWizardArm
+
+@export var book_wizard_arm_scene = preload("res://scenes/props/book_wizard_arm.tscn")
+var book_wizard
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var summon_book = get_parent().get_parent().find_child("SummonBook")
 	summon_book.connect("book_up", Callable(self, "_on_summon_book_book_up"))
 	summon_book.connect("book_down", Callable(self, "_on_summon_book_book_down"))
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	book_wizard = book_wizard_arm_scene.instantiate()
+	print(summon_book.get_parent().get_parent())
+	summon_book.get_parent().get_parent().add_child(book_wizard)
 
 func _on_summon_book_book_up():
 	#print("up")
 	wizard_arm.enabled = false
 	wizard_arm.visible = false
-	book_wizard_arm.enabled = true
-	book_wizard_arm.visible = true
+	book_wizard.enabled = true
+	book_wizard.visible = true
 
 func _on_summon_book_book_down():
 	#print("down")
 	wizard_arm.enabled = true
 	wizard_arm.visible = true
-	book_wizard_arm.enabled = false
-	book_wizard_arm.visible = false
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		book_wizard_arm.set_position(event.position*1.44)
+	book_wizard.enabled = false
+	book_wizard.visible = false
--- scenes/props/BookWizardArm.gd
+++ scenes/props/BookWizardArm.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 
 var enabled = false
+var offset = Vector2(0,250)
+var anim_sprite : AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim_sprite = find_child("AnimatedSprite2D")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if enabled:
+		if event is InputEventMouseMotion:
+			var camera_scaler = (1 / 1.44) # Scaler to account for Camera Scaling
+			set_position((event.position * camera_scaler) + offset)
+		if event is InputEventMouseButton:
+			if event.pressed:
+				anim_sprite.animation = "Grab"
+			else:
+				anim_sprite.animation = "default"
